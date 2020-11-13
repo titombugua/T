64211522f36d7325558f9b350d929489d2d9476b
Tito mbugua <iamtitombugua@gmail.com>
2020-11-13 13:44:43 +0400

    contact form

--- web/forms.py
+++ web/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm, Textarea
 from django import forms
+from django.core.mail import send_mail, BadHeaderError
 # from tinymce.widgets import TinyMCE
 from ckeditor.widgets import CKEditorWidget
 from ckeditor.fields import RichTextFormField
@@ -16,6 +17,11 @@ class StoryForm(ModelForm):
 }
 		
 
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
 
 
 
--- web/views.py
+++ web/views.py
@@ -83,3 +83,22 @@ def add_story(request):
 	else:
 		form = StoryForm()
 	return render(request, 'bf.html', {'form': form})
+
+def contactView(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['julesmbugua@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "contact_list.html", {'form': form})
+
+def successView(request):
+    return HttpResponse('Success! Thank you for your message.')
